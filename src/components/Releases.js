3e8d52174bfef8e72ceecf1a45a5c2491685a5c9
Felipe Trost <felipe.trost@gmail.com>
2020-05-30 02:33:12 -0400

    Made style adjustments for smaller devices

--- src/components/Releases.js
+++ src/components/Releases.js
@@ -3,12 +3,15 @@ import React from 'react';
 const Releases = ({tracks, trackSelected, reference})=>{
     const releases = tracks.map(value => (
         <div className="flip-card" key={value.id} onClick={() => trackSelected(value.id)}>
-            <div className="flip-card-inner">
-                <div className="flip-card-front">
-                <img src={value.cover_medium} />
-            </div>
-            <div className="flip-card-back">
-                <h3>{value.title}</h3>
+            <div className="dummy"></div>
+            <div className="content">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                    <img src={value.cover_medium} />
+                </div>
+                <div className="flip-card-back">
+                    <h3>{value.title}</h3>
+                    </div>
                 </div>
             </div>
         </div>
